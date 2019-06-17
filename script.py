556858939946eddd5921bd82692a3c2166075019
Aditya Pudipeddi <adityapudipeddi@gmail.com>
2019-06-17 16:55:29 +0530

    Added bot

--- script.py
+++ script.py
@@ -1,2 +1,16 @@
-while True:
-	print("I am working!")
+#!/usr/bin/python
+import praw
+import re
+import random
+
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("pewdiepiesubmissions")
+
+for comment in subreddit.stream.comments():
+    print(comment.body)
+    if comment.body == 'what?':
+        marvin_reply = "You've never played Tuber Simulator before?"
+        comment.reply(marvin_reply)
+        print(marvin_reply)
